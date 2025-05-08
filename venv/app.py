da387b0d94491c1030ef4550067efd3a4fe255d8
CharanReddy3211 <106661687+CharanReddy3211@users.noreply.github.com>
2025-05-08 22:36:14 +0530

    "third commit"

--- venv/app.py
+++ venv/app.py
@@ -15,7 +15,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db.init_app(app)
 
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'.
+login_manager.login_view = 'login'
 init_models(app)
 
 
